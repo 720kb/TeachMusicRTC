fcb9c99ec9676b0c218f01222d07922719a2ef6b
wouldgo <wouldgo84@gmail.com>
2015-04-11 14:42:52 +0200

    versions new

--- tasks/copy.js
+++ tasks/copy.js
@@ -8,7 +8,8 @@
     , toNewDir = 'public/vendor'
     , materialize = require(toModule + 'materialize-css/package.json').files
     , jQuery = require(toModule + 'jquery/package.json').main
-    , materializeIndex = 0
+    , sglr = require(toModule + 'siglr/package.json').files
+    , anIndex = 0
     , pathFrom
     , newFile;
 
@@ -20,11 +21,21 @@
     }
 
     fs.mkdirpSync(toNewDir);
-    for (; materializeIndex < materialize.length; materializeIndex += 1) {
+    for (; anIndex < materialize.length; anIndex += 1) {
 
-      pathFrom = path.resolve(__dirname, toModule + 'materialize-css/', materialize[materializeIndex]);
-      newFile = toNewDir + '/' + materialize[materializeIndex];
+      pathFrom = path.resolve(__dirname, toModule + 'materialize-css/', materialize[anIndex]);
+      newFile = toNewDir + '/' + materialize[anIndex];
       fs.copySync(pathFrom, newFile);
+    }
+
+    for (anIndex = 0; anIndex < sglr.length; anIndex += 1) {
+
+      if (sglr[anIndex] !== 'index.js') {
+
+        pathFrom = path.resolve(__dirname, toModule + 'siglr/', sglr[anIndex]);
+        newFile = toNewDir + '/' + sglr[anIndex];
+        fs.copySync(pathFrom, newFile);
+      }
     }
 
     fs.copySync(path.resolve(__dirname, toModule + 'jquery/', jQuery), toNewDir + '/' + jQuery);
